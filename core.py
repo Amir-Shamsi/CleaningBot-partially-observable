da56dd1631b10879bbbe35b56a1bc07be862d02a
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-07-24 14:30:36 +0430

    use model inside agent core

--- core.py
+++ core.py
@@ -1,7 +1,11 @@
+from model import Model
+
 
 class CleaningAgent(Model):
     model_filename = 'memory/bot-model-w.txt'
     def __init__(self, filename: str = None):
+        super().__init__(self.model_filename, filename)
+        self._update_model()
     def run(self):
 if __name__ == '__main__':
     action = CleaningAgent().run()
