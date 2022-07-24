9624bdeb721f63944326c2aa66a24edbb117c70e
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-07-24 14:25:22 +0430

    create class `Model`

---
+++ model.py
@@ -0,0 +1,6 @@
+
+class Model:
+    signs = {'bot': 'b', 'dirty': 'd', 'empty': '-', 'invisible': 'o'}
+
+    def __init__(self, model_filename, filename):
+        self.model_filename = model_filename
